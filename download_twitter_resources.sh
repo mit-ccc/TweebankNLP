442697870da961db2b3c5a9442ca17beb5fdbfd9
Hang Jiang <hjian42@icloud.com>
2024-01-23 23:25:30 -0500

    Update download_twitter_resources.sh

--- download_twitter_resources.sh
+++ download_twitter_resources.sh
@@ -8,6 +8,6 @@ python ./data/wordvec/English/convert_vectors.py \
 --dest_file ./twitter-stanza/data/wordvec/English/en.twitter100d.xz
 
 # download saved_models
-wget https://lsm-data.s3.amazonaws.com/ccc-hang/TweebankNLP/tweebanknlp_saved_models.zip
+wget https://ccc-hjian42.s3.amazonaws.com/public/TweebankNLP/tweebanknlp_saved_models.zip
 unzip tweebanknlp_saved_models.zip -d twitter-stanza
 rm tweebanknlp_saved_models.zip
